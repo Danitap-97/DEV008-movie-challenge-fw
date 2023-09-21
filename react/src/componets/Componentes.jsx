c91fdda21be61e4ef95c145586d60d2df7b37c89
Danitap-97 <131627619+Danitap-97@users.noreply.github.com>
2023-09-20 20:17:39 -0500

    realizo peticion usando fetch a mi api

--- react/src/componets/Componentes.jsx
+++ react/src/componets/Componentes.jsx
@@ -1,8 +1,49 @@
-
+import  { useState, useEffect } from 'react';
 
 export const Componentes = () => {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=f54cb438a37076a7b6dbcab3ee848999'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Recorremos los resultados y obtenemos los detalles de cada película
+        const moviePromises = data.results.map((movie) =>
+          fetch(
+            `https://api.themoviedb.org/3/movie/${movie.id}?api_key=f54cb438a37076a7b6dbcab3ee848999&language=en-US`
+          )
+            .then((response) => response.json())
+        );
+
+        // Esperamos a que todas las solicitudes de detalles se completen
+        Promise.all(moviePromises).then((moviesWithDetails) => {
+          setData(moviesWithDetails);
+        });
+      });
+  }, []);
+
   return (
-    <div>Componentes</div>
-    
-  )
-}
+    <div className="App">
+      <h1>SpectrumCine</h1>
+      <ul>
+        {data.map((movie) => (
+          <li key={movie.id}>
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+            <p>Fecha de lanzamiento: {movie.release_date}</p>
+            {movie.poster_path && (
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={`${movie.title} Poster`}
+              />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
